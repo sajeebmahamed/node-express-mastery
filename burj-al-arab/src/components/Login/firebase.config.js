0ef1b0002ed0fb440ad7f57bc14f18116f487cb4
Sajeeb <sajeebahamed6@gmail.com>
2020-10-13 01:25:46 +0600

    burg al

---
+++ burj-al-arab/src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBOjiCYKQx1pdt_gsenfG2TKsFA5WAZwCk",
+  authDomain: "fir-auth-with-reactjs.firebaseapp.com",
+  databaseURL: "https://fir-auth-with-reactjs.firebaseio.com",
+  projectId: "fir-auth-with-reactjs",
+  storageBucket: "fir-auth-with-reactjs.appspot.com",
+  messagingSenderId: "959572884803",
+  appId: "1:959572884803:web:449ffa944ad7904eb483f6"
+};
+  export default firebaseConfig;
