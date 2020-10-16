a1c00d6bd47ddf890dc42d21ee655162e0e48c1f
Sajeeb <sajeebahamed6@gmail.com>
2020-10-16 23:47:10 +0600

    single user by query

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const morgan = require('morgan')
+const PORT = process.env.PORT || 4000
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+
+const user = 'burjAlAdmin'
+const pass = 'sa112233'
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${user}:${pass}@cluster0.i3zel.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+client.connect(err => {
+    const bookCollection = client.db("burjAlArab").collection("booking");
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body
+        bookCollection.insertOne(newBooking)
+        .then(result => {
+            console.log(result);
+            res.send(result.insertedCount > 0)
+        })
+
+    })
+    app.get('/bookings', (req, res) => {
+        bookCollection.find({ email: req.query.email})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
---
+++ src/components/Booking/Booking.js
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
+
+const Booking = () => {
+    const [booking, setBooking] = useState([])
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    console.log(booking);
+    useEffect(() => {
+        fetch('http://localhost:4000/bookings?email='+loggedInUser.email)
+            .then(res => res.json())
+            .then(data => setBooking(data))
+    }, [])
+    return (
+        <div>
+           {
+               booking.map(book => (
+                   <div>
+                       <p> Your total booking: {book.length} </p>
+                       <p> Your booking is: {(new Date(book.checkIn).toDateString('dd/MM/yyyy'))} and {book.checkOut} </p>
+                   </div>
+               ))
+           }
+        </div>
+    );
+};
+
+export default Booking;
